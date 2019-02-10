64fb7b76349e79154aed9266827ffa008f9918fa
emBrileg08 <46973724+emBrileg08@users.noreply.github.com>
2019-02-10 10:18:58 -0500

    Updated from Brython Server: 2/10/2019 10:18:58 AM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,10 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: emBrileg08
+Credit: Used this link: https://stackoverflow.com/questions/5618878/how-to-convert-list-to-string
+to figure out how to make a list a string
+Used this link: https://stackoverflow.com/questions/15418561/convert-a-word-to-a-list-of-chars
+to figure out how to convert a string into a list of individual words
 
 Assignment:
 
@@ -25,5 +28,15 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-string=input("Please enter a string of text (the bigger the better): ")
-print('You entered "'+str(string)+'" . Now jumble it:')
+text=input("Please enter a string of text (the bigger the better): ")
+print('You entered "'+str(text)+'". Now jumble it:')
+letrev=list(text[::-1]) #converts the text into a list of letters, then reverses the order
+letrevstring= ''.join(letrev) #converts the previous list into a string
+print(letrevstring) #prints the string
+word=text.split(" ") #Splits the inputted text into a list where each word is an item in the list
+wordrev=word[::-1] #reverses the order of the previous list
+wordrevstring=' '.join(wordrev) #makes the previous list a string
+print(wordrevstring)
+third=[(''.join(list(x[::-1]))) for x in word] #Uses the split word list, splits each word in the list into its characters, reverses the characters, then joins each list of characters back into a string
+thirdstr=' '.join(third) #Stringifys the previous
+print(thirdstr)
